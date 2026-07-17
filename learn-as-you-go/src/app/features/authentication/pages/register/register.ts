d866807964c2d7a84951609d0c9fe8ed5c936dbc
almas-kai <almas.kairbek.ak@gmail.com>
2026-07-17 12:19:27 +0500

    Update packages (FE), and start implementing the register

--- learn-as-you-go/src/app/features/authentication/pages/register/register.ts
+++ learn-as-you-go/src/app/features/authentication/pages/register/register.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { TranslateModule } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    TranslateModule
+  ],
   templateUrl: './register.html',
   styleUrl: './register.scss'
 })
 export class Register {
-
+  private readonly dialogRef = inject(MatDialogRef);
 }
