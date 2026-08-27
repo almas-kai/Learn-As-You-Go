f6e28b67a621398ff6bf7292322ba02f13a3dacd
almas-kai <almas.kairbek.ak@gmail.com>
2026-08-28 00:03:43 +0500

    add first test

--- learn-as-you-go/src/app/features/authentication/register-modal/register-modal.spec.ts
+++ learn-as-you-go/src/app/features/authentication/register-modal/register-modal.spec.ts
@@ -1,11 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RegisterModal } from './register-modal';
-import { MatDialogRef } from '@angular/material/dialog';
-import { TranslateModule } from '@ngx-translate/core';
+import { MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { By } from '@angular/platform-browser';
+
+const TRANSLATION_MOCKS = {
+  authentication: {
+    register: {
+      title: 'Register'
+    }
+  }
+} as const;
 
 describe(RegisterModal.name, () => {
   let component: RegisterModal;
   let fixture: ComponentFixture<RegisterModal>;
+  let loader: HarnessLoader;
+  let translateService: TranslateService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,6 +28,7 @@ describe(RegisterModal.name, () => {
       ],
       providers: [
         {
+          // Faking MatDialogRef cause if i were to import a real one, it would break the app (contains too many deps).
           provide: MatDialogRef,
           useValue: {
             close: () => {}
@@ -25,11 +39,25 @@ describe(RegisterModal.name, () => {
     .compileComponents();
 
     fixture = TestBed.createComponent(RegisterModal);
+
+    translateService = TestBed.inject(TranslateService);
+    translateService.setTranslation('en', TRANSLATION_MOCKS);
+    translateService.use('en');
+
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
+
     await fixture.whenStable();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display correct title text', () => {
+    const titleElement = fixture.debugElement.query(By.directive(MatDialogTitle)).nativeElement as HTMLHeadingElement;
+
+    expect(titleElement).toBeDefined();
+    expect(titleElement.textContent.trim()).toBe(translateService.instant('authentication.register.title'));
   });
 });
